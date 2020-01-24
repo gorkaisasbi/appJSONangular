3fde18c7fba04139a1ec730e0d381509813a7d75
Gorka <dw2_alum9@inform.local>
2020-01-24 13:05:49 +0100

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import postres from "../assets/desserts.json";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'appJSONangular';
+
+  postres : any[] = postres.data;
+
+  constructor(){
+    console.log(postres.data);
+  }
+
 }
